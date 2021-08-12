8141b1fed0c61ad1bde6b635cf75a87cefb67258
jinnethCh <jixiagredo92@gmail.com>
2021-08-11 22:53:29 -0500

    metodos añadidos

--- src/model/Wallet3.java
+++ src/model/Wallet3.java
@@ -7,6 +7,7 @@ public class Wallet3 {
      */
     private int saldo;
     private boolean tieneLimite;
+    private int meta;
 
     public Wallet3(boolean limite) {
         super();
@@ -25,7 +26,26 @@ public class Wallet3 {
                 return "No puede superar el limte";
         } 
         saldo += valor;
+        if(verificarMeta()){System.out.println("has superado la meta");}
         return "operacion exitosa, nuevo saldo " + saldo;
+    }
+
+    public String definirMeta(int valor){
+        if(valor > limiteBilletera && tieneLimite){
+            return "No puedes establecer una meta mas alta que el limite";
+        }
+        if(valor <= saldo){
+            return "ya has superado";
+        }
+        meta = valor;
+        return "nueva meta agregada!";
+    }
+
+    public boolean verificarMeta(){
+        if(saldo >= meta && meta >= 0){
+            return true;
+        }
+        return false;
     }
 }
 //git add .
